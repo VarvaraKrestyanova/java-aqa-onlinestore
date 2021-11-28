9a65abf7066fbd7807b4bb5b79b2bd95d750aa6e
VarvaraKrestyanova <krestyanovav@gmail.com>
2021-11-28 17:35:38 +0300

    003_oop - removed unused params

--- online-store/domain/src/main/java/com/issoft/auto/domain/Category.java
+++ online-store/domain/src/main/java/com/issoft/auto/domain/Category.java
@@ -6,8 +6,6 @@ public class Category {
 
     private String name;
     private List<Product> products;
-    private int numberOfProducts;
-    private String productName;
 
     public Category(String name) {
         this.name = name;
@@ -17,32 +15,12 @@ public class Category {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public List<Product> getProducts() {
         return products;
     }
 
     public void setProducts(List<Product> products) {
         this.products = products;
-    }
-
-    public int getNumberOfProducts() {
-        return numberOfProducts;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setNumberOfProducts(int numberOfProducts) {
-        this.numberOfProducts = numberOfProducts;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
     }
 
 }
